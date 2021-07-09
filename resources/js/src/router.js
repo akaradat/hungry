89a8f40a1567ef55354ecd1a47bab98f187e0f15
Akrd kmnc <akrd.kmnc@gmail.com>
2021-07-09 17:41:18 +0700

    Initial commit

---
+++ resources/js/src/router.js
@@ -0,0 +1,44 @@
+/*=========================================================================================
+  File Name: router.js
+  Description: Routes for vue-router. Lazy loading is enabled.
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
+    },
+    routes: [
+        {
+            path: "",
+            component: () => import("@/layouts/full-page/FullPage.vue"),
+            children: [
+                {
+                    path: "*",
+                    name: "restaurant",
+                    component: () => import("@/views/pages/Login.vue")
+                }
+            ]
+        }
+    ]
+});
+
+router.afterEach(() => {
+    // Remove initial loading
+    const appLoading = document.getElementById("loading-bg");
+    if (appLoading) {
+        appLoading.style.display = "none";
+    }
+});
+
+export default router;
---
+++ resources/js/src/views/pages/Restaurant.vue
@@ -0,0 +1,44 @@
+<!-- =========================================================================================
+    File Name: Login.vue
+    Description: Login Page
+    ----------------------------------------------------------------------------------------
+    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+      Author: Pixinvent
+    Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+
+<template>
+  <section>HELLO WORLD</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      checkbox_remember_me: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#page-login {
+  .social-login-buttons {
+    .bg-facebook {
+      background-color: #1551b1;
+    }
+    .bg-twitter {
+      background-color: #00aaff;
+    }
+    .bg-google {
+      background-color: #4285f4;
+    }
+    .bg-github {
+      background-color: #333;
+    }
+  }
+}
+</style>
