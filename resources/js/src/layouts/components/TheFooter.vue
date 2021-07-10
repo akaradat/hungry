d9356704c7e998c8b4e3e819f37de1f339ffb5ae
Akrd kmnc <akrd.kmnc@gmail.com>
2021-07-10 22:14:11 +0700

    style: update header and footer

--- resources/js/src/layouts/components/TheFooter.vue
+++ resources/js/src/layouts/components/TheFooter.vue
@@ -10,22 +10,31 @@
 
 
 <template functional>
-    <footer class="the-footer flex-wrap justify-between" :class="classes">
-        <span>COPYRIGHT &copy; {{ new Date().getFullYear() }} <a href="https://1.envato.market/vuexy_admin" target="_blank" rel="nofollow">Pixinvent</a>, All rights Reserved</span>
-        <span class="md:flex hidden items-center">
-            <span>Hand-crafted & Made with</span>
-            <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
-        </span>
-    </footer>
+  <footer class="the-footer flex-wrap justify-between" :class="classes">
+    <span>HUNGRY SEARCHER</span>
+    <span class="md:flex hidden items-center">
+      <span
+        >By
+        <a href="https://www.linkedin.com/in/kawmaneechot-a/" target="_blank"
+          >AKRD KMNC</a
+        ></span
+      >
+      <feather-icon
+        icon="HeartIcon"
+        svgClasses="stroke-current text-danger w-6 h-6"
+        class="ml-2"
+      />
+    </span>
+  </footer>
 </template>
 
 <script>
 export default {
-    name: "the-footer",
-    props: {
-        classes: {
-            type: String,
-        },
-    }
-}
+  name: "the-footer",
+  props: {
+    classes: {
+      type: String,
+    },
+  },
+};
 </script>
--- resources/js/src/layouts/main/Main.vue
+++ resources/js/src/layouts/main/Main.vue
@@ -22,7 +22,7 @@
             class="vx-logo cursor-pointer mx-auto flex items-center"
           >
             <logo class="w-10 mr-4 fill-current text-primary" />
-            <span class="vx-logo-text text-primary">Restaurant</span>
+            <span class="vx-logo-text text-primary">Hungry</span>
           </router-link>
         </vs-navbar>
       </div>
